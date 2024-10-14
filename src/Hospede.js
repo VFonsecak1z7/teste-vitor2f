0eff5ee7383bb9a5b3d1a47e89c6ef7cffb7d29a
VFonsecak1z7 <95054352+VFonsecak1z7@users.noreply.github.com>
2024-10-14 00:19:20 -0300

    Update Hospede.js
    *Implementação da classe Hospede que representa um hóspede no sistema.
    *Contem validação de nome e email
    *A classe possui métodos de validação para nome (não vazio) e email (formato correto).
    *Métodos getter/setters implementados para nome e email.
    *Comentarios especificos e referencias de linhas de codigo foram escritas como comentarios dentro do file

--- src/Hospede.js
+++ src/Hospede.js
@@ -1,15 +1,44 @@
 /**
  * Classe que representa um hóspede.
- * Os candidatos devem implementar essa classe para gerenciar os dados do hóspede.
  */
 class Hospede {
     /**
      * @param {string} nome - Nome do hóspede.
      * @param {string} email - Email do hóspede.
      */
-
     constructor(nome, email) {
         // TODO: Implementar o construtor para inicializar as propriedades nome e email
+        this.setNome(nome);    //Setter do nome
+        this.setEmail(email);  //Setter do email
+    } 
+
+    /** 
+     * Validacao e definicao do NOME do hospede.
+     * @param {string} nome - Nome do hóspede.
+     */
+    setNome(nome){
+        //Basicamente verifica se o input na varivel `nome` eh string ou esta vazia.
+        if(typeof nome !== 'string' || nome.trim === ''){
+            //Utilizei o metodo `trim()` para remocao dos espacos em branco
+            throw new Error('NOME NAO EH VALIDO! Favor entrar com um nome que nao seja em branco')
+        }
+
+        this.nome = nome;
+    }
+
+    /** 
+     * Validacao e definicao do EMAIL do hospede.
+     * @param {string} email - Email do hóspede.
+     */
+    setEmail(email){
+        //Trecho retirado do site https://mailtrap.io/blog/javascript-email-validation/
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)){
+            //Testa se o padrao para email eh atendido caso retorne FALSE retorna a mensagem de erro.
+            throw new Error('EMAIL NAO EH VALIDO! Favor entrar com um email valido.')
+        }else{
+            this.email = email;
+        }
     }
 
     /**
@@ -18,6 +47,7 @@ class Hospede {
      */
     getNome() {
         // TODO: Implementar o método para retornar o nome do hóspede
+        return this.nome;
     }
 
     /**
@@ -26,9 +56,8 @@ class Hospede {
      */
     getEmail() {
         // TODO: Implementar o método para retornar o email do hóspede
+        return this.email;
     }
 }
 
 export default Hospede;
-
-
